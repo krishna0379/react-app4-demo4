36aa145036be78569a4131361c0e3384a75f1ba4
krishna0379 <iamkrishna0379@gmail.com>
2021-06-03 07:46:46 +0530

    counter

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,42 @@
+import { useState } from "react";
+
 export function Home() {
-    return (
-      <div className="bg-primary" style={{ height: "100vh" }}>
-        <h1 className="text-light">Home Page</h1>
+  const [tinput, setTinput] = useState("");
+  const updateTinput = (e) => setTinput(e.target.value);
+
+  const [list, setList] = useState([]);
+  const postTweet = () => {
+    setList([tinput, ...list]);
+    setTinput("");
+  };
+
+  return (
+    <div>
+      {/** CAPTURE INPUT HERE  */}
+      <div className="d-flex mt-2 mx-4">
+        <input
+          className="form-control w-75"
+          type="text"
+          value={tinput}
+          onChange={(e) => updateTinput(e)}
+          placeholder="tweer here"
+        />
+        <input
+          type="button"
+          onClick={() => postTweet()}
+          className="w-25 btn btn-secondary"
+          value="Tweet"
+        />
       </div>
-    );
-  }
+
+      {/** DISPLAY TWEET HERE */}
+      <div className="mx-4 mt-4">
+        {list.map((item, index) => (
+          <h4 key={index} className="text-secondary">
+            {item}
+          </h4>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 export function Project() {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => setCounter(counter + 1);
+
   return (
-    <div className="bg-success" style={{ height: "100vh" }}>
-      <h1 className="text-light">Project Page</h1>
+    <div className="mx-4">
+      <div className="alert alert-info mt-2 text-center">COUNTER {counter}</div>
+
+      <input
+        className="btn btn-secondary btn-lg w-100"
+        type="button"
+        onClick={() => increment()}
+        value="Counter"
+      />
     </div>
   );
 }
